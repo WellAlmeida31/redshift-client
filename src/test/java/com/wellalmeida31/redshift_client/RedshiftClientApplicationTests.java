e14749b53a5c24154771ab1f0d2342cb45dc1366
Wellington.Almeida <wellington.almeida@omnik.com.br>
2025-05-09 19:30:41 -0300

    Initial commit

---
+++ src/test/java/com/wellalmeida31/redshift_client/RedshiftClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellalmeida31.redshift_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedshiftClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
